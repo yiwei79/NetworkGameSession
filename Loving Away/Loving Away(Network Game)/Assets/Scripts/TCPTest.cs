ff367774c3bd12dfe8ea0a2dfac9487728b80330
Yiwei Ye <125266017+Yeeway69@users.noreply.github.com>
2025-10-08 12:10:21 +0200

    start sending message

--- Loving Away/Loving Away(Network Game)/Assets/Scripts/TCPTest.cs
+++ Loving Away/Loving Away(Network Game)/Assets/Scripts/TCPTest.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 public class TCPTest : MonoBehaviour
 {
@@ -11,10 +12,11 @@ public class TCPTest : MonoBehaviour
 
     void Start()
     {
-      ServerProcess();
-
       m_clientThread = new Thread(ClientProcess);
       m_serverThread = new Thread(ServerProcess);
+
+      m_serverThread.Start();
+      m_clientThread.Start();
     }
 
     void ServerProcess()
@@ -24,8 +26,56 @@ public class TCPTest : MonoBehaviour
       serverSocket.Bind(endPoint);
       serverSocket.Listen(10);
       Debug.Log("Server is running on port 9050");
+      
+      Socket clientHandler = serverSocket.Accept();
+      Debug.Log("Client accepted");
 
-      m_clientThread.Start();
+      //Recieve message from client
+      byte[] buffer = new byte[1024];
+      int bytesRead = clientHandler.Receive(buffer);
+      string message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+      Debug.Log("Client sent message: " + message);
+
+      //Send message to client
+      string response = "Message received";
+      byte[] responseBuffer = System.Text.Encoding.UTF8.GetBytes(response);
+      clientHandler.Send(responseBuffer);
+      Debug.Log("Server say Hello Back to Client");
+
+      clientHandler.Close();
+    }
+
+    void ClientProcess()
+    {
+        Thread.Sleep(1000);
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback,9050);
+        try
+        {
+            // Connect to the server
+            clientSocket.Connect(serverEndPoint);
+            Debug.Log("Client connected to server!");
+            
+            // Send a message
+            byte[] message = System.Text.Encoding.UTF8.GetBytes("Hello from client!");
+            clientSocket.Send(message);
+            Debug.Log("Client sent message");
+
+            //Recieve message from server
+            byte[] buffer = new byte[1024];
+            int bytesRead = clientSocket.Receive(buffer);
+            string message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Debug.Log("Server sent message: " + message);
+            
+        }
+        catch (SocketException e)
+        {
+            Debug.LogError($"Client connection failed: {e.Message}");
+        }
+        finally
+        {
+            clientSocket.Close();
+        }
     }
 
     void OnApplicationQuit()
@@ -34,6 +84,15 @@ public class TCPTest : MonoBehaviour
         {
             serverSocket.Close();
         }
+        
+        if(m_serverThread != null && m_serverThread.IsAlive)
+        {
+            m_serverThread.Abort();
+        }
+        
+        if(m_clientThread != null && m_clientThread.IsAlive)
+        {
+            m_clientThread.Abort();
+        }
     }
-
 }
